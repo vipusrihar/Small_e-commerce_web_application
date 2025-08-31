c32690f2044a1d210291fd42fdd8b6b84ee4e815
vipusrihar <vipusrihar@gmail.com>
2025-08-31 18:03:07 +0530

    backend initialized

---
+++ backend/src/main/java/com/vipusa/securebackend/SecurebackendApplication.java
@@ -0,0 +1,13 @@
+package com.vipusa.securebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurebackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vipusa/securebackend/SecurebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vipusa.securebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
